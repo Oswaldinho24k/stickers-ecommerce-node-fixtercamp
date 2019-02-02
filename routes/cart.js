b943922548af8fc17b8d2ff2edb0ba099162bcac
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2019-02-02 16:03:17 -0600

    ya quedó el carrito :car:

--- routes/cart.js
+++ routes/cart.js
@@ -1,4 +1,27 @@
 const router = require('express').Router() //parenthesis needed
+const Product = require('../models/Product')
+
+
+//mostrar el carrito
+router.get('/', (req, res)=>{
+  const {cart} = req.app.locals
+  Product.find({_id:{$in:Object.keys(cart)}})
+    .then(products=>{
+      console.log(products)
+      let total = 0
+      products = products.map(p=>{
+        total += p.price * cart[p._id]
+        let product = {
+          product:p,
+          quantity:cart[p._id]
+        }
+        return product
+      })
+      res.render('cart',{products, total})
+    }).catch(e=>res.render('error'))
+
+})
+
 
 router.post('/add', (req, res)=>{
   const {cart} = req.app.locals
@@ -10,7 +33,7 @@ router.post('/add', (req, res)=>{
 
 //restar
 
-//mostrar el carrito
+
 
 
 
